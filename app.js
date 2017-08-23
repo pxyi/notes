e6464b19fcb9d0ff3c5fd858c241992cdb92e914
phuhoang <phuhoang@sina.com>
2017-08-23 11:54:12 +0800

    新增gzip压缩和缓存, 使用koa-static-cache作为静态文件中间件

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const Koa = require('koa');
 const app = new Koa();
 
-/* Nunjucks模板引擎, 根据环境控制是否缓存 */
-let templating = require('./templating');
-const isProduction = process.env.NODE_ENV === 'production';
-app.use(templating('views', {
-    noCache: !isProduction,
-    watch: !isProduction
-}));
 
-/* 设置静态文件路径 */
-let staticFiles = require('./static-files');
-app.use(staticFiles('/static/', __dirname + '/static'));
+
+
+/* 记录URL以及页面执行时间 */
+app.use(async (ctx, next) => {
+    let start = new Date().getTime();
+    await next();
+    let execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
+});
+
+/* 是否为开发环境 */
+const isProduction = process.env.NODE_ENV === 'production';
+
+var gzip = require('koa-gzip');
+app.use(gzip());
+
+/* 设置静态文件路径及缓存时间 */
+var staticCache = require('koa-static-cache');
+var maxAge = isProduction ? 24 * 60 * 60 * 30 : 0;
+app.use(staticCache('./static', { maxAge: maxAge}));
 
 /* 设置获取post请求参数中间件 */
 const bodyparser = require('koa-bodyparser');
 app.use(bodyparser());
+
+/* Nunjucks模板引擎, 根据环境控制是否缓存 */
+let templating = require('./config/templating');
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
+}));
 
 /* 设置路由 */
 const routes = require('./routes/routes');
